1a8120abe90f542b10446e8123f30764cfafc0a6
Razmik <zenfonezd551@gmail.com>
2023-12-04 21:28:01 +0300

    add code for task solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[5] { "123", "hello", "23", "res", "world" };
+string[] array2 = new string[array1.Length];
+
+void NewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine($"{array[i]}");
+    }
+    System.Console.WriteLine();
+}
+
+NewArray(array1, array2);
+PrintArray(array2);
